69b0f9c463cd1c73b3d61a47aaf963c1b512f5ac
Keruthiga <krithy08.me@gmail.com>
2023-09-29 21:02:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filter from "./components/Filters/Filter";
+import Card from "./components/Cards/Card";
+import React, { useState, useEffect} from 'react';
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
+import CardDetails from "./components/Cards/CardDetails";
+
+
 
 function App() {
   return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<CardDetails />} />
+
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episodes/:id" element={<CardDetails />} />
+
+        <Route path="/location" element={<Location />} />
+        <Route path="/location/:id" element={<CardDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+const Home = () => {
+  let [pageNumber, setPageNumber ] = useState(1);
+  let [search, setSearch] = useState(" ");
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
+  let [fetchedData, updateFetchedData] = useState([]);
+  let {info , results} = fetchedData;
+  
+  
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+  useEffect(()=>{
+    (async function(){
+      let data = await fetch(api).then(res=> res.json())
+      updateFetchedData(data);
+    })()
+  },[api])
+
+
+
+
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="text-center mb-3">Characters</h1>
+      <Search  setPageNumber={setPageNumber} setSearch={setSearch}/>
+      <div className="container">
+        <div className="row">
+        <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            setPageNumber={setPageNumber}
+          />
+          <div className="col-lg-8 col-12"> 
+            <div className="row"> <Card page="/" results= {results}/> </div>
+          </div>
+        </div>
+      </div>
+      <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
+    </div> 
   );
 }
 
